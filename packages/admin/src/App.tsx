747820fd69c2af904119430c501c2924b14795eb
rainbowbear <rainbow96bear@gmail.com>
2024-05-23 11:37:37 +0900

    update : root에 axios 추가, shared의 SampleComponent 적용

--- packages/admin/src/App.tsx
+++ packages/admin/src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import SampleComponent from "shared/dist/index";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div>admin</div>
+        <SampleComponent />
       </header>
     </div>
   );
--- packages/shared/dist/index.js
+++ packages/shared/dist/index.js
@@ -1,11 +1,11 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sampleFunc = void 0;
-// import _ from 'lodash';
-// export const capitalize = (str: string) => {
-//   return _.capitalize(str);
-// };
-var sampleFunc = function (str) {
-  return "this is input string = " + str;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-exports.sampleFunc = sampleFunc;
+Object.defineProperty(exports, "__esModule", { value: true });
+// export하여 admin과 client등 다른 곳에서 사용할 유틸리티 함수 작성
+var react_1 = __importDefault(require("react"));
+var SampleComponent = function () {
+    return react_1.default.createElement("div", null, "sample Component");
+};
+exports.default = SampleComponent;
