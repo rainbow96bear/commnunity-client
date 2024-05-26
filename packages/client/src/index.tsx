d6e991fd1922a8fa464ee4161c47ce31c2279a75
rainbowbear <rainbow96bear@gmail.com>
2024-05-26 22:25:57 +0900

    update : Kakao OAuth 작업

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -14,13 +14,10 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<App />}></Route>
-        <Route path="/Login" element={<Login />}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- packages/client/src/pages/Login/Login.tsx
+++ packages/client/src/pages/Login/Login.tsx
@@ -1,12 +1,29 @@
+import axios from "axios";
 import { Box, LoginButtonBox, LoginButton, Title } from "./Login.style";
 
 const Login = () => {
-  // fix : OAuth 로그인 로직 추가 필요
+  const handleLogin = async (platform: string) => {
+    let oAuthURL = "";
+    switch (platform) {
+      case "kakao":
+        oAuthURL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_RESTFUL_API_KEY}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}&response_type=code`;
+    }
+    try {
+      if (oAuthURL == "") {
+        alert("지원하지 않는 로그인 방식입니다.");
+      } else {
+        window.location.href = oAuthURL;
+      }
+    } catch (error) {
+      console.error("Error during Kakao login:", error);
+    }
+  };
+
   return (
     <Box>
       <Title>Login</Title>
       <LoginButtonBox>
-        <LoginButton>카카오</LoginButton>
+        <LoginButton onClick={() => handleLogin("kakao")}>카카오</LoginButton>
       </LoginButtonBox>
     </Box>
   );
